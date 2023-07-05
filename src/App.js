e886627350baab1252024b67d46a93086d4a0e04
Juan Correa <ema8720@gmail.com>
2023-07-04 22:12:39 -0700

    The App Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from "react";
-import logo from "./logo.svg";
+import NucampLogo from "./app/assets/img/logo.png";
+import { Container, Navbar, NavbarBrand } from "reactstrap";
 
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello Nucamp!</div>;
+  return (
+    <div className="App">
+      <Navbar dark color="primary" sticky="top" expand="md">
+        <Container>
+          <NavbarBrand href="/">
+            <img src={NucampLogo} alt="nucamp logo"></img>
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+      I'm ready for workshop!
+    </div>
+  );
 }
 
 export default App;
